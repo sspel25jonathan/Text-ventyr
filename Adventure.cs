c3f4491f9d6d6a431f9374e367b97bfeb8890160
jonathan <sspel25jope@axevalla.fhsk.se>
2025-10-07 11:49:46 +0200

    wow

---
+++ Adventure.cs
@@ -0,0 +1,105 @@
+using System;
+using System.Data;
+using System.Runtime.InteropServices.Marshalling;
+using System.Text.RegularExpressions;
+
+namespace TextAdventure
+
+{
+    public class Adventure
+    {
+        public static void Main(string[] args)
+        {
+            Random rng = new Random();
+            int hp = 6;
+            int FoeHp;
+            string[] Inv = { "[1]Tortch ", "[2]Healthpotion " };
+            string[] foeNameGrenrator = { "Bear", "ill" };
+            int RandomFoe = rng.Next(2);
+            int RandomDamage = rng.Next(1, 3);
+            string FoeFacing = foeNameGrenrator[RandomFoe];
+            if (FoeFacing == "Bear")
+            {
+                FoeHp = 6;
+            }
+            else
+            {
+                FoeHp = 5;
+            }
+            ;
+            Console.WriteLine("You are faceing face to face agenst " + FoeFacing);
+
+
+            for (int i = 0; i < hp;)
+            {
+                Console.WriteLine("What will you do?");
+                Console.WriteLine("[1]Attack [2]Items");
+
+                int choice = Convert.ToInt32(Console.ReadLine());
+                int tempi = 0;
+                while (tempi < 1)
+                {
+                    if (choice == 1)
+                    {
+                        FoeHp = FoeHp - RandomDamage;
+                        tempi = 1;
+                        Console.WriteLine("FoeHp is now " + FoeHp);
+                    }
+                    else if (choice == 2)
+                    {
+                        foreach (string b in Inv)
+                        {
+                            Console.Write(b);
+
+                        }
+                        Console.Write("[3]Return to other options");
+                        int Tempchoice = Convert.ToInt32((Console.ReadLine()));
+                        if (Tempchoice == 1)
+                        {
+                            tempi = 1;
+                            if (FoeFacing == "Bear")
+                            {
+                                Console.WriteLine("The Bear ran away you win!");
+                            }
+                            else
+                            {
+                                Console.WriteLine("It did nothing");
+                            }
+                        }
+                        else if (Tempchoice == 2)
+                        {
+                            tempi = 1;
+                            Console.WriteLine("You gain 2 hp");
+                            hp = hp + 2;
+                        }
+                        else
+                        {
+                            tempi = 1;
+                            continue;
+                        }
+                    }
+                }
+                while (FoeHp > 0)
+                {
+                    Console.WriteLine("You got attacked");
+                    hp = hp - RandomDamage;
+                    Console.WriteLine("your hp is now " + hp);
+                    break;
+                }
+                if (FoeHp <= 0)
+                {
+                    Console.WriteLine("You won");
+                    break;
+            }
+            if (hp <= 0)
+                {
+                    Console.WriteLine(" you lost");
+                    break;
+            }
+                
+            }
+            
+
+        }
+    }
+}
